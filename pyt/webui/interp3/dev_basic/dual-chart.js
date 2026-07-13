8c970465c15f1151dcc7fa4d95700879f84fa7d5
Oracle Public Cloud User <opc@a1-ryu.subnet10301047.vcn10301047.oraclevcn.com>
2026-07-13 02:09:48 +0000

    interpolate engine

--- pyt/webui/interp3/dev_basic/dual-chart.js
+++ pyt/webui/interp3/dev_basic/dual-chart.js
@@ -158,3 +158,31 @@ function buildInputOutputMeta(dataA, dataB, opts = {}) {
 	}
 	return { inputMeta, outputMeta };
 }
+
+function plotBoth() {
+	const { inputMeta, outputMeta } = buildInputOutputMeta(grid.getData(), grid_2.getData(), {
+		labelA: 'Input',
+		labelB: 'Output'
+	});
+	chart.renderInputOutput(grid.getData(), grid_2.getData(), { inputMeta, outputMeta });
+}
+
+// 👇 INSERT: pulls query X's from Output grid's column A
+function getQueryXs() {
+	return grid_2.getData()
+		.map(row => row[0])
+		.filter(v => v !== '' && v !== undefined && v !== null)
+		.map(Number)
+		.filter(x => !Number.isNaN(x));
+}
+
+// 👇 INSERT: computes Output from Input, writes it back, then plots
+function interpolateAndPlot() {
+	const inputData = grid.getData();
+	const queryXs = getQueryXs();
+	if (!queryXs.length) { plotBoth(); return; }
+
+	const outputTable = InterpEngine.buildOutputTable(inputData, queryXs);
+	grid_2.setData(outputTable);
+	plotBoth();
+}
